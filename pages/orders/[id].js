673adc628a25561f4d878e66e3013136502cc504
Drilon <drilonsulejmani305@gmail.com>
2021-12-28 18:49:04 +0100

    designed shopping cart

---
+++ pages/orders/[id].js
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+import styles from '../../styles/Order.module.css'
+
+
+const Order = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Order
